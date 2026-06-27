c64efedd24747ab4eca7f71ca675ac097a0ddc2c
Shlomi Israely <shlomi.israely@gmail.com>
2026-06-27 17:12:52 +0200

    refactor(professionals): migrate services hook to zustand store

--- src/packages/professionals/hooks/use-services.ts
+++ src/packages/professionals/hooks/use-services.ts
@@ -1,26 +1,26 @@
 import React from "react";
 import lodash from "lodash";
 
-import { useQueryString } from "./use-query-string";
+import { useProfessionalsQuery, useSetProfessionalsQuery } from "../store";
 import { services } from "../config";
 
 export function useServices() {
-  const [query, setQuery] = useQueryString();
+  const query = useProfessionalsQuery();
+  const setQuery = useSetProfessionalsQuery();
 
   React.useEffect(() => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       if (query.services === null) return;
 
-      const cleanedServices = lodash.intersection(
-        query.services,
-        Object.keys(services)
-      );
+      const cleanedServices = lodash.intersection(query.services, Object.keys(services));
 
       if (cleanedServices.length !== query.services.length) {
         setQuery({ services: cleanedServices });
       }
     }, 10);
-  }, []);
+
+    return () => clearTimeout(timer);
+  }, [query.services, setQuery]);
 
   const addService = (id: string) => {
     const theService = services[id];
